929fe29062bf6ac65f99d74a53d6c7ef0c91d1c8
Egor Semenov <Hinodem@mail.ru>
2023-10-05 22:07:33 +0300

    WorkPC last edit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -39,11 +40,11 @@ def special(char_name: str, char_class: str) -> str:
 
 def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        print(f'{char_name: str}, ты Воитель — отличный боец ближнего боя.')
+        print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
-        print(f'{char_name: str}, ты Маг — превосходный укротитель стихий.')
+        print(f'{char_name}, ты Маг — превосходный укротитель стихий.')
     if char_class == 'healer':
-        print(f'{char_name: str}, ты Лекарь'
+        print(f'{char_name}, ты Лекарь'
               f' — чародей, способный исцелять раны.')
     print('Потренируйся управлять своими навыками.')
     print(
@@ -65,8 +66,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    approve_choice:str = ''
-    char_class:str = ''
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input(
             'Введи название персонажа, за которого хочешь играть:'
@@ -92,15 +93,17 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(
         f'Здравствуй, {char_name}! '
         'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.',
     )
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+
